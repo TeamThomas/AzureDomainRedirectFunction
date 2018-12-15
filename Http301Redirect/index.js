0c62af2d50825afa1caff232a73504d64011f8d5
Sven Vollbehr <sven.vollbehr@thomasconcretegroup.com>
2018-12-15 19:05:59 +0100

    Add project files

---
+++ Http301Redirect/index.js
@@ -0,0 +1,31 @@
+module.exports = async function (context, req) {
+    if (process.env["RedirectRules"] === undefined) {
+        context.log.info("Functions.Http301Redirect: No redirect rules found.");
+    } else {
+        try {
+            var disguisedHost = req.headers["disguised-host"];
+            if (disguisedHost === undefined) {
+                disguisedHost = "localhost";
+            }
+
+            var redirectRules = JSON.parse(process.env["RedirectRules"]);
+            var redirectRule = redirectRules[disguisedHost];
+            if (redirectRule === undefined && disguisedHost.startsWith("www.")) {
+                redirectRule = redirectRules[disguisedHost.subst(4)];
+            }
+            if (redirectRule === undefined) {
+                context.log.info("Functions.Http301Redirect: No redirect rules found for " + disguisedHost + ".");
+            } else {
+                context.log.info("Functions.Http301Redirect: Redirecting " + disguisedHost + " to " + redirectRules[disguisedHost]);
+                context.res = {
+                    status: 301,
+                    headers: {
+                        "Location": redirectRules[disguisedHost]
+                    }
+                };
+            }
+        } catch (e) {
+            context.log.error(e)
+        }
+    }
+};
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# AzureDomainRedirectFunction
-Listens to http/80 and produces a HTTP 301 redirect to a permanent URL.
+# Azure Domain Redirect Function
+Azure DNS lacks a HTTP Redirect capability. Azure Domain Redirect Function supplements Azure DNS with HTTP Redirect capability. The function listens to http/80 and produces a HTTP 301 redirect to a permanent URL. The domain value is read from `DISGUISED-HOST` environment value.
+
+## Application Settings
+
+### RedirectRules
+This setting controls which domain is redirected to which URL.
+
+Here is an example setting value in `local.settings.json`.
+
+```json
+{
+  "IsEncrypted": false,
+  "Values": {
+    "RedirectRules": "{\"localhost\":\"http:\/\/www.microsoft.com\/\"}"
+  }
+}
+```
+
+
